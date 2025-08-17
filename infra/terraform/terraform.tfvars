cc58117fc79d90987661648aef09de94df7d3ab5
Sumanth0660 <skonannagari0660@conestogac.on.ca>
2025-08-16 21:12:58 -0400

    updated

--- infra/terraform/terraform.tfvars
+++ infra/terraform/terraform.tfvars
@@ -1,8 +1,17 @@
-aws_region       = "us-east-1"
-key_pair_name    = "FINALCICD2"        # or whatever key you created
-ssh_cidr         = "24.141.163.126/32" # e.g., "99.88.77.66/32"
-app_port         = 3000                # must match how your app listens
+app_name        = "cicd-final"
+aws_region      = "us-east-1"
+vpc_cidr        = "10.0.0.0/16"
+public_subnet_1 = "10.0.1.0/24"
+public_subnet_2 = "10.0.2.0/24"
 assign_public_ip = true
+ssh_cidr        = "0.0.0.0/0"
 
-ecr_repo_url = "019773930547.dkr.ecr.us-east-1.amazonaws.com/cicd-final-backend"
-image_tag    = "latest"
+app_port        = 3000
+backend_port    = 3000
+backend_tg_name = "app-tg"
+
+ecr_repo_url    = "019773930547.dkr.ecr.us-east-1.amazonaws.com/cicd-final-backend"
+image_tag       = "latest"
+
+key_pair_name   = ""     # leave empty to skip SSH key
+public_key_path = ""     # leave empty to skip SSH key
--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,24 +1,73 @@
-# variables.tf
-variable "repo_name" {
-  description = "ECR repo name for the backend image"
-  type        = string
+# App & image
+variable "app_name" {
+  type    = string
+  default = "cicd-final"
 }
-
+variable "app_port" {
+  type    = number
+  default = 3000
+}
+variable "ecr_repo_url" {
+  type    = string
+  default = "019773930547.dkr.ecr.us-east-1.amazonaws.com/cicd-final-backend"
+}
 variable "image_tag" {
-  description = "Docker image tag to deploy"
-  type        = string
-  default     = "latest"
+  type    = string
+  default = "latest"
 }
+variable "repo_name" {
+  type    = string
+  default = "cicd-final-backend"
+} # harmless if unused
 
+# AWS / instance
 variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
+  type    = string
+  default = "us-east-1"
+}
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+variable "assign_public_ip" {
+  type    = bool
+  default = true
 }
 
-# Optional, if your code references it
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.micro"
+# Networking
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+variable "public_subnet_1" {
+  type    = string
+  default = "10.0.1.0/24"
+}
+variable "public_subnet_2" {
+  type    = string
+  default = "10.0.2.0/24"
+}
+variable "ssh_cidr" {
+  type    = string
+  default = "0.0.0.0/0"
+} # tighten later
+
+# EC2 key pair (optional; set empty to skip)
+variable "key_pair_name" {
+  type    = string
+  default = ""
+}
+variable "public_key_path" {
+  type    = string
+  default = ""
+}
+
+# ALB / backend
+variable "backend_port" {
+  type    = number
+  default = 3000
+}
+variable "backend_tg_name" {
+  type    = string
+  default = "app-tg"
 }
